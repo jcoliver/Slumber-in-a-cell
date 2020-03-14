0987666c4da74678925cf059653aa3efd821a97e
katbeescience <mkbusby@email.arizona.edu>
2020-03-13 17:52:46 -0700

    Update file name for .csv file, make minor compatibility edits as needed.

--- LMER_bee_random_effect.R
+++ LMER_bee_random_effect.R
@@ -3,10 +3,10 @@
 # 10/30/2019
 # mkbusby@email.arizona.edu
 
-# Load libraries.
+# Install packages. Load libraries.
 
-install.packages("lmerTest")
-install.packages("emmeans")
+# install.packages("lmerTest")
+# install.packages("emmeans")
 library(lme4)
 library(lmerTest)
 library(emmeans)
@@ -16,22 +16,22 @@ library(tidyverse)
 
 # Load data file. 
 
-mydata <- read.csv(file = "[Insert path to New Clicks Only.csv here]",
+mydata <- read.csv(file = "[Insert path to Vent_times.csv here]",
                    header = TRUE,
                    sep = ",")
 
 # Select the relevant columns only.
 
 tidydf <- mydata %>%
-  select(Behavior, Bee, EvSep..3) %>%
-  filter(EvSep..3<1000)
+  select(Behavior, Bee, EvSepby3) %>%
+  filter(EvSepby3<1000)
 
 # Do the test.
 
-lmerEvSep <- lmer(EvSep..3 ~ Behavior + (1|Bee), tidydf)
+lmerEvSep <- lmer(EvSepby3 ~ Behavior + (1|Bee), tidydf)
 summary(lmerEvSep)
 
-glmEvSep <- glm(EvSep..3~Behavior+Bee, data=tidydf)
+glmEvSep <- glm(EvSepby3~Behavior+Bee, data=tidydf)
 summary(glmEvSep)
 
 # Code from this point on is from David Reineke and then edited by Kathryn Busby:
@@ -39,7 +39,7 @@ summary(glmEvSep)
 # anova with ranked data (see R-Boggers)
 # https://www.r-bloggers.com/beware-the-friedman-test/ 
 
-mod7r <- lmer(EvSep..3~Behavior + (1|Bee), data=tidydf)
+mod7r <- lmer(EvSepby3~Behavior + (1|Bee), data=tidydf)
 
 # hypothesis tests
 anova(mod7r) # F tests fixed effects
@@ -54,20 +54,18 @@ hist(residuals(mod7r))
 boxplot(residuals(mod7r), main="Boxplot of mod7r residuals")
 qqnorm(residuals(mod7r))
 qqline(residuals(mod7r))
-shapiro.test(residuals(mod7r))
 
 # only do this if residuals are not normal
 # model using ranked data
 
-tidydf$EvSep..3.r <- rank(tidydf$EvSep..3)
-mod7r.r <- lmer(EvSep..3.r~Behavior + (1|Bee), data=tidydf)
+tidydf$EvSepby3.r <- rank(tidydf$EvSepby3)
+mod7r.r <- lmer(EvSepby3.r~Behavior + (1|Bee), data=tidydf)
 
 plot(mod7r.r, main="Plot mod7r to check residuals for normality")
 hist(residuals(mod7r.r))
 boxplot(residuals(mod7r.r), main="Boxplot of mod7r residuals")
 qqnorm(residuals(mod7r.r))
 qqline(residuals(mod7r.r))
-shapiro.test(residuals(mod7r.r))
 
 # hypothesis tests now on newly transformed model (mod7r.r)
 anova(mod7r.r) # F tests fixed effects
@@ -82,19 +80,19 @@ emmeans.r <- emmeans(mod7r.r, pairwise~Behavior, adjust="Tukey", lmer.df="satter
 
 sink(file="[Insert desired output file path here.]")
 
-cat("Our data are not normally distributed, so we've applied a ____ transformation.\n
-    The following analyses are performed after a rank transformation:\n")
+cat("Our data are not normally distributed, so we've applied a rank transformation.\n
+    The following analyses are performed after a rank transformation:\n\n")
 
-cat("The following describes our linear mixed model fit:\n")
+cat("The following describes our linear mixed model fit:\n\n")
 print(mod7r.r)
 
-cat("This shows differences between fixed effects:\n")
+cat("This shows differences between fixed effects:\n\n")
 print(anova(mod7r.r))
 
-cat("This shows differences between random effects:\n")
+cat("This shows differences between random effects:\n\n")
 print(ranova(mod7r.r))
 
-cat("multiple comparisons:\n")
+cat("multiple comparisons:\n\n")
 print(emmeans.r)
 
 sink()

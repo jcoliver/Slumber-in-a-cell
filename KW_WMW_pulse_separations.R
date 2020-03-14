767d055e1813caa0180456c68057bcd28dacc2d9
katbeescience <mkbusby@email.arizona.edu>
2020-03-13 18:04:39 -0700

    Update .csv file path and minor compatibility edits.

--- KW_WMW_pulse_separations.R
+++ KW_WMW_pulse_separations.R
@@ -8,14 +8,14 @@ library(tidyverse)
 
 # Bring in data file.
 
-mydata <- read.csv(file = "[Insert your path to New Clicks Only.csv here]",
+mydata <- read.csv(file = "[Insert your path to Vent_times.csv here]",
                    header = TRUE,
                    sep = ",")
 
 # Eliminate those NAs that occurred where there wasn't an event separation, at the beginning of each behavior.
 
 shortdata <- mydata %>%
-  select(Behavior, EvSep..3)
+  select(Behavior, EvSepby3)
 
 shortdata <- na.omit(shortdata)
 
@@ -23,44 +23,45 @@ shortdata <- na.omit(shortdata)
 # This should be looking within bouts. So first index to <1000.
 
 pulses <- shortdata %>%
-  filter(EvSep..3 < 1000)
+  filter(EvSepby3 < 1000)
 
 # Kruskal-Wallis tests for pulse separations between behaviors.
 # These data are all right skewed, so not normally distributed.
 
-kruskal.test(EvSep..3 ~ Behavior, data=pulses)
+kruskal.test(EvSepby3 ~ Behavior, data=pulses)
 
 # Next we need to test for differences between specific pairs of behaviors.
 # Create variables to index to them individually.
 
 dv.pulse <- pulses %>%
   filter(Behavior == "abdomen dv") %>%
-  filter(EvSep..3 < 1000)
+  filter(EvSepby3 < 1000)
 
 cleaner.pulse <- shortdata %>%
   filter(Behavior == "cleaner") %>%
-  filter(EvSep..3 < 1000)
+  filter(EvSepby3 < 1000)
 
 feeder.pulse <- shortdata %>%
   filter(Behavior == "feeder") %>%
-  filter(EvSep..3 < 1000)
+  filter(EvSepby3 < 1000)
 
 heater.pulse <- shortdata %>%
   filter(Behavior == "heater") %>%
-  filter(EvSep..3 < 1000)
+  filter(EvSepby3 < 1000)
 
 # Now we need to do the non-parametric pairwise comparisons that correspond to the KW test, which would
 # be Wilcoxon-Mann-Whitney tests of all the following pairs:
 
 # cleaner-abdomen dv
-wilcox.test(dv.pulse$EvSep..3, cleaner.pulse$EvSep..3)
+wilcox.test(dv.pulse$EvSepby3, cleaner.pulse$EvSepby3)
 # feeder-abdomen dv  
-wilcox.test(dv.pulse$EvSep..3, feeder.pulse$EvSep..3)
+wilcox.test(dv.pulse$EvSepby3, feeder.pulse$EvSepby3)
 # heater-abdomen dv   
-wilcox.test(dv.pulse$EvSep..3, heater.pulse$EvSep..3)
+wilcox.test(dv.pulse$EvSepby3, heater.pulse$EvSepby3)
 # feeder-cleaner     
-wilcox.test(feeder.pulse$EvSep..3, cleaner.pulse$EvSep..3)
+wilcox.test(feeder.pulse$EvSepby3, cleaner.pulse$EvSepby3)
 # heater-cleaner 
-wilcox.test(heater.pulse$EvSep..3, cleaner.pulse$EvSep..3)
+wilcox.test(heater.pulse$EvSepby3, cleaner.pulse$EvSepby3)
 # heater-feeder  
-wilcox.test(heater.pulse$EvSep..3, feeder.pulse$EvSep..3)
+wilcox.test(heater.pulse$EvSepby3, feeder.pulse$EvSepby3)
+
